c5c5c7a93cd3d2b43238ca20b89af1590d33c753
Gerardo <gerardo.rosas@external.danone.com>
2023-04-09 14:49:55 -0600

    add 4 escenarios

---
+++ 4-escenarios/Completar párrafo/js/parrafo.js
@@ -0,0 +1,88 @@
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let idElemento;
+    if (navigator.userAgent.indexOf('Firefox') !== -1) {
+        idElemento = ev.target.id;
+    } else {
+        idElemento = ev.toElement.id;
+    }
+    console.log("idElemento", idElemento)
+    let posicion = document.getElementById(idElemento);
+    posicion.style.padding = '0px';
+    posicion.innerHTML = '';
+
+    let posiciones = document.getElementById(idElemento).children;
+
+    if (posiciones.length > 0) {
+        swal({
+            title: "Error.",
+            text: "Solo se permite una respuesta en la " + idElemento,
+            icon: "error"
+        });
+    } else {
+        let itemId = idElemento.substr(0, idElemento.length - 1);
+        console.log("itemId: ", itemId)
+        if ('posicion' === itemId) {
+            var data = ev.dataTransfer.getData("text");
+            ev.target.appendChild(document.getElementById(data));
+        } else {
+            swal({
+                title: "Error.",
+                text: "Solo puedes colocar la respuesta dentro de una casilla ",
+                icon: "error"
+            });
+        }
+    }
+}
+
+function validarRespuesta() {
+    let numPosicion = 3;
+
+    let posicionesRespuestas = [
+        'respuesta1',
+        'respuesta2',
+        'respuesta3',
+    ];
+
+    let correctas = [];
+
+
+    for (let index = 0; index < posicionesRespuestas.length; index++) {
+        let num = index + 1;
+        let itemPosicionId = 'posicion' + num;
+        const element = document.getElementById(itemPosicionId).children;
+
+        if (element[0]) {
+
+            if (posicionesRespuestas[index] === element[0].getAttribute('id')) {
+                correctas.push(posicionesRespuestas[index])
+            }
+
+        }
+    }
+    if (correctas.length === numPosicion) {
+        swal({
+            title: "Exito",
+            text: "Las respuestas son correctas.",
+            icon: "success"
+        });
+    } else {
+        swal({
+            title: "Error.",
+            text: "Las respuestas son incorrectas.",
+            icon: "error"
+        });
+    }
+
+}
+
